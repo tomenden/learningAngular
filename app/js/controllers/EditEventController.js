f485864794ace30b5f1e8355f0b72872d0f4bd07
Tom Enden <tome@wix.com>
2015-09-06 16:11:45 +0300

    Validation

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,8 +2,11 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
-        $scope.saveEvent = function (event) {
-            window.alert('event name: ' + event.name);
+        $scope.saveEvent = function (event, newEventForm) {
+            console.log(newEventForm);
+            if (newEventForm.$valid) {
+                window.alert('event name: ' + event.name);
+            }
         };
         $scope.cancelEdit = function () {
             window.location = 'EventDetails.html';
